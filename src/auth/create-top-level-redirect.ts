dd401033d391d2d75de936e757dce68d3b334244
Ricardo Pineda <pinedasoft@gmail.com>
2019-07-17 02:58:48 -0600

    Digitalframe release

---
+++ src/auth/create-top-level-redirect.ts
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+
+import querystring from 'querystring';
+import {Context} from 'koa';
+
+import redirectionPage from './redirection-page';
+
+dotenv.config();
+
+const production_host = process.env.PRODUCTION_HOST;
+
+export default function createTopLevelRedirect(path: string) {
+  return function topLevelRedirect(ctx: Context) {
+    const {host, query} = ctx;
+    const {shop} = query;
+
+    const params = {shop};
+    const queryString = querystring.stringify(params);
+
+    ctx.body = redirectionPage({
+      origin: `https://${shop}`,
+      redirectTo: `https://${host}${path}?${queryString}`,
+    });
+  };
+}
